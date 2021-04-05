52a419fcb0a6c7fc0e629e71b5fe6f632018dcbc
RGB Studios <justingolden21@gmail.com>
2021-04-05 16:52:27 -0700

    added long variations
    this code could be cleaned up better, but it's good for now
    
    next up is figuring out how to remove the eyes, then the final step of exporting for print (maybe a canvas for png? pdf would be nice too...)

--- js/scripts.js
+++ js/scripts.js
@@ -24,12 +24,20 @@ window.onload = ()=> {
 	for(let key of names)
 		html += `<div class="art" style="background-color:${colors.dark}">${removeEyes(changeArt(art[key],artID++,-1,colors.white),key,5)}</div>`;
 
-	html += `<div class="art" style="background-color:${colors.white}">${getAll(artID++)}</div>`;
-	html += `<div class="art" style="background-color:${colors.dark}">${getAll(artID++)}</div>`;
-	html += `<div class="art" style="background-color:${colors.dark}">${getAll(artID++,colors.dark,colors.white)}</div>`;
-	html += `<div class="art" style="background-color:${colors.white}">${getAll(artID++,colors.white,colors.dark)}</div>`;
-	html += `<div class="art" style="background-color:${colors.black}">${getAll(artID++,-1,-1,0)}</div>`;
-	html += `<div class="art" style="background-color:${colors.black}">${getAll(artID++,-1,colors.white)}</div>`;
+	html += `<div class="art square" style="background-color:${colors.white}">${getAll(artID++)}</div>`;
+	html += `<div class="art square" style="background-color:${colors.dark}">${getAll(artID++)}</div>`;
+	html += `<div class="art square" style="background-color:${colors.dark}">${getAll(artID++,colors.dark,colors.white)}</div>`;
+	html += `<div class="art square" style="background-color:${colors.white}">${getAll(artID++,colors.white,colors.dark)}</div>`;
+	html += `<div class="art square" style="background-color:${colors.black}">${getAll(artID++,-1,-1,0)}</div>`;
+	html += `<div class="art square" style="background-color:${colors.black}">${getAll(artID++,-1,colors.white)}</div>`;
+
+	html += `<div class="art long" style="background-color:${colors.white}">${getAllLong(artID++)}</div>`;
+	html += `<div class="art long" style="background-color:${colors.dark}">${getAllLong(artID++)}</div>`;
+	html += `<div class="art long" style="background-color:${colors.dark}">${getAllLong(artID++,colors.dark,colors.white)}</div>`;
+	html += `<div class="art long" style="background-color:${colors.white}">${getAllLong(artID++,colors.white,colors.dark)}</div>`;
+	html += `<div class="art long" style="background-color:${colors.black}">${getAllLong(artID++,-1,-1,0)}</div>`;
+	html += `<div class="art long" style="background-color:${colors.black}">${getAllLong(artID++,-1,colors.white)}</div>`;
+
 
 	u('#art').html(html);
 };
@@ -46,4 +54,5 @@ const removeEyes = (svg,name,num) => svg.replaceAll('</style>','#'+name+num+' .e
 // const getTextBetweenStrings = (str, startStr, endStr) => str.substring( str.lastIndexOf(startStr) + startStr.length, str.lastIndexOf(endStr) );
 
 const removeSVGElement = svg => svg.replace('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 398.66 686.65">','').replace('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 561.77 824.2">','').replace('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 796.62 851.94">','').replace('</svg>','');
-const getAll = (num, fill=-1, stroke=-1, width=-1)=> '<svg viewbox="0 0 1608 1608"><g transform="translate(400 835)">' + changeArt(removeSVGElement(art.litwick),num,fill,stroke,width) + '</g><g transform="translate(120 80)">' + changeArt(removeSVGElement(art.lampent),num,fill,stroke,width) + '</g><g transform="translate(700 120)">' + changeArt(removeSVGElement(art.chandelure),num,fill,stroke,width) + '</g></svg>';
+const getAll = (num, fill=-1, stroke=-1, width=-1)=> '<svg viewbox="0 0 1600 1600"><g transform="translate(400 835)">' + changeArt(removeSVGElement(art.litwick),num,fill,stroke,width) + '</g><g transform="translate(120 80)">' + changeArt(removeSVGElement(art.lampent),num,fill,stroke,width) + '</g><g transform="translate(700 120)">' + changeArt(removeSVGElement(art.chandelure),num,fill,stroke,width) + '</g></svg>';
+const getAllLong = (num, fill=-1, stroke=-1, width=-1)=> '<svg viewbox="0 0 2100 1400"><g transform="translate(60 460)">' + changeArt(removeSVGElement(art.litwick),num,fill,stroke,width) + '</g><g transform="translate(620 440)">' + changeArt(removeSVGElement(art.lampent),num,fill,stroke,width) + '</g><g transform="translate(1220 340)">' + changeArt(removeSVGElement(art.chandelure),num,fill,stroke,width) + '</g></svg>';
