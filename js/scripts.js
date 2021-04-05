9427e2dae0f31cf9524f8c962e58de9958d0a1bb
RGB Studios <justingolden21@gmail.com>
2021-04-05 16:39:07 -0700

    add all in one

--- js/scripts.js
+++ js/scripts.js
@@ -9,17 +9,24 @@ const colors = {
 }
 
 window.onload = ()=> {
+	let artID = 0;
 	let html = '';
 	for(let key of names)
 		html += `<div class="art">${art[key]}</div>`;
 	for(let key of names)
-		html += `<div class="art" style="background-color:${colors.dark}">${changeArt(art[key],2,colors.dark,colors.white)}</div>`;
+		html += `<div class="art" style="background-color:${colors.dark}">${changeArt(art[key],artID++)}</div>`;
 	for(let key of names)
-		html += `<div class="art" style="background-color:${colors.white}">${changeArt(art[key],3,colors.white,colors.dark)}</div>`;
+		html += `<div class="art" style="background-color:${colors.dark}">${changeArt(art[key],artID++,colors.dark,colors.white)}</div>`;
 	for(let key of names)
-		html += `<div class="art" style="background-color:${colors.black}">${changeArt(art[key],4,-1,-1,0)}</div>`;
+		html += `<div class="art" style="background-color:${colors.white}">${changeArt(art[key],artID++,colors.white,colors.dark)}</div>`;
 	for(let key of names)
-		html += `<div class="art" style="background-color:${colors.dark}">${removeEyes(changeArt(art[key],5,-1,colors.white),key,5)}</div>`;
+		html += `<div class="art" style="background-color:${colors.black}">${changeArt(art[key],artID++,-1,-1,0)}</div>`;
+	for(let key of names)
+		html += `<div class="art" style="background-color:${colors.dark}">${removeEyes(changeArt(art[key],artID++,-1,colors.white),key,5)}</div>`;
+
+	html += `<div class="art">${getAll()}</div>`;
+	html += `<div class="art" style="background-color:${colors.dark}">${getAll()}</div>`;
+
 	u('#art').html(html);
 };
 
@@ -33,3 +40,6 @@ const removeEyes = (svg,name,num) => svg.replace('</style>','#'+name+num+' .eye{
 // const removeEyes = (svg,name,num) => svg.replace(getTextBetweenStrings(svg, '<path class="eye', '/>'),'');
 
 // const getTextBetweenStrings = (str, startStr, endStr) => str.substring( str.lastIndexOf(startStr) + startStr.length, str.lastIndexOf(endStr) );
+
+const removeSVGElement = svg => svg.replace('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 398.66 686.65">','').replace('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 561.77 824.2">','').replace('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 796.62 851.94">','').replace('</svg>','');
+const getAll = ()=> '<svg viewbox="0 0 1608 1608"><g transform="translate(400 835)">' + removeSVGElement(art.litwick) + '</g><g transform="translate(120 80)">' + removeSVGElement(art.lampent) + '</g><g transform="translate(700 120)">' + removeSVGElement(art.chandelure) + '</g></svg>';
