40e394c2fe459ab0bc2f0f6e252631bb6b985fa7
RGB Studios <justingolden21@gmail.com>
2021-04-05 16:43:21 -0700

    adding more with all three

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ window.onload = ()=> {
 	let artID = 0;
 	let html = '';
 	for(let key of names)
-		html += `<div class="art">${art[key]}</div>`;
+		html += `<div class="art" style="background-color:${colors.white}">${art[key]}</div>`;
 	for(let key of names)
 		html += `<div class="art" style="background-color:${colors.dark}">${changeArt(art[key],artID++)}</div>`;
 	for(let key of names)
@@ -24,8 +24,12 @@ window.onload = ()=> {
 	for(let key of names)
 		html += `<div class="art" style="background-color:${colors.dark}">${removeEyes(changeArt(art[key],artID++,-1,colors.white),key,5)}</div>`;
 
-	html += `<div class="art">${getAll()}</div>`;
-	html += `<div class="art" style="background-color:${colors.dark}">${getAll()}</div>`;
+	html += `<div class="art" style="background-color:${colors.white}">${getAll(artID++)}</div>`;
+	html += `<div class="art" style="background-color:${colors.dark}">${getAll(artID++)}</div>`;
+	html += `<div class="art" style="background-color:${colors.dark}">${getAll(artID++,colors.dark,colors.white)}</div>`;
+	html += `<div class="art" style="background-color:${colors.white}">${getAll(artID++,colors.white,colors.dark)}</div>`;
+	html += `<div class="art" style="background-color:${colors.black}">${getAll(artID++,-1,-1,0)}</div>`;
+	html += `<div class="art" style="background-color:${colors.black}">${getAll(artID++,-1,colors.white)}</div>`;
 
 	u('#art').html(html);
 };
@@ -36,10 +40,10 @@ const changeStroke = (svg, stroke) => stroke == -1 ? svg : svg.replaceAll(/strok
 const changeStrokeWidth = (svg, width) => width == -1 ? svg : svg.replaceAll(/stroke-width:10px/g, 'stroke-width:'+width+'px');
 const changeArt = (svg, num, fill=-1, stroke=-1, width=-1) => changeStrokeWidth(changeStroke(changeFill(changeNum(svg,num),fill),stroke),width);
 
-const removeEyes = (svg,name,num) => svg.replace('</style>','#'+name+num+' .eye{opacity:0;display:none;fill:none;stroke:none;stroke-width:0}</style>');
+const removeEyes = (svg,name,num) => svg.replaceAll('</style>','#'+name+num+' .eye{opacity:0;display:none;fill:none;stroke:none;stroke-width:0}</style>');
 // const removeEyes = (svg,name,num) => svg.replace(getTextBetweenStrings(svg, '<path class="eye', '/>'),'');
 
 // const getTextBetweenStrings = (str, startStr, endStr) => str.substring( str.lastIndexOf(startStr) + startStr.length, str.lastIndexOf(endStr) );
 
 const removeSVGElement = svg => svg.replace('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 398.66 686.65">','').replace('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 561.77 824.2">','').replace('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 796.62 851.94">','').replace('</svg>','');
-const getAll = ()=> '<svg viewbox="0 0 1608 1608"><g transform="translate(400 835)">' + removeSVGElement(art.litwick) + '</g><g transform="translate(120 80)">' + removeSVGElement(art.lampent) + '</g><g transform="translate(700 120)">' + removeSVGElement(art.chandelure) + '</g></svg>';
+const getAll = (num, fill=-1, stroke=-1, width=-1)=> '<svg viewbox="0 0 1608 1608"><g transform="translate(400 835)">' + changeArt(removeSVGElement(art.litwick),num,fill,stroke,width) + '</g><g transform="translate(120 80)">' + changeArt(removeSVGElement(art.lampent),num,fill,stroke,width) + '</g><g transform="translate(700 120)">' + changeArt(removeSVGElement(art.chandelure),num,fill,stroke,width) + '</g></svg>';
