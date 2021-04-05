5536661b59018c9f91869cfab29b4115f6365a89
RGB Studios <justingolden21@gmail.com>
2021-04-04 22:31:21 -0700

    updating change art functions

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,12 @@ window.onload = ()=> {
 		html += `<div class="art">${art[key]}</div>`;
 	}
 	for(let key of names) {
-		html += `<div class="art" style="background-color:${colors.background}">${changeArtNum(art[key],2).replaceAll(/fill:#([0-9a-f]{6})/ig, 'fill:'+colors.background).replaceAll(/stroke:#000/ig, 'stroke:'+colors.light)}</div>`;
+		html += `<div class="art" style="background-color:${colors.background}">${changeArt(art[key],2,colors.background,colors.light)}</div>`;
 	}
 	u('#art').html(html);
 };
 
-const changeArtNum = (str, num) => str.replaceAll('litwick','litwick'+num).replaceAll('lampent','lampent'+num).replaceAll('chandelure','chandelure'+num);
+const changeNum = (svg, num) => svg.replaceAll('litwick','litwick'+num).replaceAll('lampent','lampent'+num).replaceAll('chandelure','chandelure'+num);
+const changeFill = (svg, fill) => svg.replaceAll(/fill:#([0-9a-f]{6})/ig, 'fill:'+fill).replaceAll(/fill:#([0-9a-f]{3})/ig, 'fill:'+fill);
+const changeStroke = (svg, stroke) => svg.replaceAll(/stroke:#000/ig, 'stroke:'+stroke);
+const changeArt = (svg, num, fill, stroke) => changeStroke(changeFill(changeNum(svg,num),fill),stroke);
